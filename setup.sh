a4eeca622ce2fa3aa6fa82ca02853cdc31c543cb
josephtsukkaiith <md18resch01003@iith.ac.in>
2018-12-09 20:08:30 +0530

    added required images

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 unzip train2014.zip
+rm train2014.zip
